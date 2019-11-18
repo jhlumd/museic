e7b72a4474524db9f05d68a02a8f31eb2f6d1c50
Jaehyuk Lee <jhlumd@gmail.com>
2019-11-18 11:55:16 -0800

    Before backend user auth

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const db = require("./config/keys").mongoURI;
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
-
+const users = require("./routes/api/users");
+const snippets = require("./routes/api/snippets");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -14,12 +15,14 @@ mongoose
 
 app.get("/", (req, res) => res.send("Testing!!"));
 
+app.use(passport.initialize());
+require("./config/passport")(passport);
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-
-
-
-
+app.use("/api/users", users);
+app.use("/api/snippets", snippets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
