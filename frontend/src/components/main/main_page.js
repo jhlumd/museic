b3bd391e348fb94d789215bb0fa0bede996bb149
Phil-1041 <philzheng92@gmail.com>
2019-11-18 18:23:05 -0800

    Frontend User Auth Complete

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -5,7 +5,7 @@ class MainPage extends React.Component {
   render() {
     return (
       <div>
-        <h1>A Twitter Clone</h1>
+        <h1>Museic</h1>
         <footer>
           Copyright &copy; 2019 Chirper
         </footer>
--- routes/api/users.js
+++ routes/api/users.js
@@ -37,7 +37,7 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
-            const payload = { id: user.id, name: user.name };
+            const payload = { id: user.id, username: user.username };
 
             jwt.sign(
               payload,
