11b5a7e73951e8dd8c5694642d799c62b5456829
Jaehyuk Lee <jhlumd@gmail.com>
2019-11-19 17:25:54 -0800

    Add snippet_form skeleton

--- frontend/src/components/snippets/upload/snippet_form.js
+++ frontend/src/components/snippets/upload/snippet_form.js
@@ -1,6 +1,42 @@
-import React from "react";
-// import Tone from "tone??";
-import Tone from "tone";
+import React, { Component } from 'react';
+// import Tone from "tone";
 
-const synth = new Tone.Synth();
+export default class SnippetForm extends Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            title: "",
+            description: "",
+            public: true,
+            notes: []
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentDidUpdate(prevProps) {
+
+    }
+    
+    
+    render() {
+        return (
+            <div className="snippet-form-container">
+                
+
+
+                
+            </div>
+        );
+    }
+}
