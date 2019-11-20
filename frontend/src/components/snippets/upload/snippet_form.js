b1fa0b80e0f22e7a8a1d146cca14ce6fa08a20f8
Jaehyuk Lee <jhlumd@gmail.com>
2019-11-19 18:18:00 -0800

    Add component did mount and piano html to snippet form

--- frontend/src/components/snippets/upload/snippet_form.js
+++ frontend/src/components/snippets/upload/snippet_form.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-// import Tone from "tone";
+import Tone from "tone";
 
+
+const synth = new Tone.Synth();
 export default class SnippetForm extends Component {
     constructor(props) {
         super(props);
@@ -21,7 +23,30 @@ export default class SnippetForm extends Component {
     }
 
     componentDidMount() {
+        const synth = new Tone.Synth();
+        synth.oscillator.type = "sine";
+        synth.toMaster();
 
+        const piano = document.getElementById("piano");
+
+        piano.addEventListener("mousedown", e => {
+            synth.triggerAttack(e.target.dataset.note);
+
+            console.log(e.timeStamp);
+            console.log(e.target.dataset);
+            console.log(e.target.dataset.note);
+
+            // this.state.notes.push({
+            //     pitch: e.target.dataset.note,
+            //     startTime: 0,
+            //     duration: 0.2
+            // });
+        });
+
+        piano.addEventListener("mouseup", e => {
+            synth.triggerRelease();
+            // console.log(e);
+        });
     }
 
     componentDidUpdate(prevProps) {
@@ -33,7 +58,34 @@ export default class SnippetForm extends Component {
             <div className="snippet-form-container">
                 
 
-
+                <ul id="piano">
+                    <li data-note="C4" className="key">
+                        <div data-note="C#4" className="black-key">R</div>
+                        D
+                    </li>
+                    <li data-note="D4" className="key">
+                        <div data-note="D#4" className="black-key">T</div>
+                        F
+                    </li>
+                    <li data-note="E4" className="key">
+                        G
+                    </li>
+                    <li data-note="F4" className="key">
+                        <div data-note="F#4" className="black-key">U</div>
+                        H
+                    </li>
+                    <li data-note="G4" className="key">
+                        <div data-note="G#4" className="black-key">I</div>
+                        J
+                    </li>
+                    <li data-note="A4" className="key">
+                        <div data-note="A#4" className="black-key">O</div>
+                        K
+                    </li>
+                    <li data-note="B4" className="key">
+                        L
+                    </li>
+                </ul>
                 
             </div>
         );
