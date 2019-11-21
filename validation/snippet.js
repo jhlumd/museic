1f6eaf68a23bcbf36e69a7e32a1758fb50089611
Jaehyuk Lee <jhlumd@gmail.com>
2019-11-20 17:51:19 -0800

    minor changes for merge with jenny

--- validation/snippet.js
+++ validation/snippet.js
@@ -10,6 +10,8 @@ module.exports = function validateSnippetInput(data) {
     errors.title = 'Title cannot be empty';
   }
 
+  // add validators for other fields (google validator github)
+
   return {
     errors,
     isValid: Object.keys(errors).length === 0
