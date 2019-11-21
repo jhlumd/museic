f86934024fb4f09f74a0b954022beee6a1992aab
Jaehyuk Lee <jhlumd@gmail.com>
2019-11-21 13:12:29 -0800

    Add minor edits to root, entities, comments reducers

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
-import commentsReducer from './comments_reducer';
+import snippets from "./snippets_reducer";
+import comments from './comments_reducer';
 
 export default combineReducers({
-  comments: commentsReducer
-})
+  snippets,
+  comments
+});
