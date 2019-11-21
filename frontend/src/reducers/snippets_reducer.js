5a151fcb43e477d37b7f237c1b825c284c4113ff
Jaehyuk Lee <jhlumd@gmail.com>
2019-11-21 13:21:15 -0800

    Add skeleton to snippets reducer

--- frontend/src/reducers/snippets_reducer.js
+++ frontend/src/reducers/snippets_reducer.js
@@ -1,12 +1,27 @@
-import { RECEIVE_ONE_SNIPPET } from "../actions/snippet_actions";
+import {
+    RECEIVE_SNIPPETS,
+    RECEIVE_USER_SNIPPETS,
+    RECEIVE_ONE_SNIPPET,
+    REMOVE_SNIPPET
+} from "../actions/snippet_actions";
 
 const snippetsReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
     let newState;
 
     switch (action.type) {
+        case RECEIVE_SNIPPETS:
+            newState = {  };
+            return Object.assign({}, oldState, newState);
+        case RECEIVE_USER_SNIPPETS:
+            newState = {  };
+            return Object.assign({}, oldState, newState);
         case RECEIVE_ONE_SNIPPET:
+            debugger;
             newState = { [action.snippet.id]: action.snippet };
+            return Object.assign({}, oldState, newState);
+        case REMOVE_SNIPPET:
+            // delete;
             return Object.assign({}, oldState, newState);
         default:
             return oldState;
