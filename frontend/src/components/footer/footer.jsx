5b815e149e07ed8fc192098f5f21d488251b0ec0
Jiani Fan <jiani.fan@gmail.com>
2019-11-22 10:30:35 -0800

    Make footer component

---
+++ frontend/src/components/footer/footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Logo from '../resources/logo';
+import GithubIcon from '../resources/github_icon';
+
+export default function Footer() {
+  return (
+    <div id='footer'>
+      <div className='footer-right-container'>
+        <Logo />
+        <p>2019</p>
+        <p>Made in SF</p>
+      </div>
+
+      <div className='footer-left-container'>
+        <GithubIcon />
+      </div>
+    </div>
+  )
+}
