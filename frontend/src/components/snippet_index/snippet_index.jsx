3c95017761a74212abc649d1848bf681a099e3f6
Jiani Fan <jiani.fan@gmail.com>
2019-11-23 21:28:30 -0800

    Style index page, index card

--- frontend/src/components/snippet_index/snippet_index.jsx
+++ frontend/src/components/snippet_index/snippet_index.jsx
@@ -37,45 +37,48 @@ class SnippetIndex extends React.Component {
     } = this.props
 
     return(
-      <div>
-        <h1>Snippet Index</h1>
-        {
-          this.props.snippets.map( snippet => {
-            if (this.state.selectedId === snippet._id) {
-              return <ShowCard 
-              key={snippet._id}
-              
-              //data needed to display and send to actions
-              snippet={snippet}
-              comments={comments}
-              likes={likes}
-              userId={userId}
-              
-              //comment actions
-              composeComment={composeComment}
-              removeComment={removeComment}
-              editComment={editComment}
-              
-              //get data on mount
-              fetchSnippetComments={fetchSnippetComments}
-              fetchSnippetOwner={fetchSnippetOwner}
-              
-              //like actions
-              newLike={newLike}
-              unlike={unlike}
-              getSnippetLikes={getSnippetLikes}
-              />
-            } else {
-              return <IndexCard
-              handleClick={ this.handleClick }
-              key={snippet._id} 
-              snippetId={snippet._id}
-              snippet={snippet}
-              />
-            }
-          
-          })
-        }
+      <div id='snippet-index-container'>
+        {/* <h1>Snippet Index</h1> */}
+
+        <div className='snippet-index-snippets-container'>
+          {
+            this.props.snippets.map( snippet => {
+              if (this.state.selectedId === snippet._id) {
+                return <ShowCard 
+                key={snippet._id}
+                
+                //data needed to display and send to actions
+                snippet={snippet}
+                comments={comments}
+                likes={likes}
+                userId={userId}
+                
+                //comment actions
+                composeComment={composeComment}
+                removeComment={removeComment}
+                editComment={editComment}
+                
+                //get data on mount
+                fetchSnippetComments={fetchSnippetComments}
+                fetchSnippetOwner={fetchSnippetOwner}
+                
+                //like actions
+                newLike={newLike}
+                unlike={unlike}
+                getSnippetLikes={getSnippetLikes}
+                />
+              } else {
+                return <IndexCard
+                handleClick={ this.handleClick }
+                key={snippet._id} 
+                snippetId={snippet._id}
+                snippet={snippet}
+                />
+              }
+            
+            })
+          }
+        </div>
       </div>
     )
   }
--- frontend/src/components/snippet_index/snippet_index_card.jsx
+++ frontend/src/components/snippet_index/snippet_index_card.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
+import SnippetDisplayContainer from '../snippet_display/snippet_display_container';
 
 class SnippetIndexCard extends React.Component {
 
   render(){
     const {snippetId, snippet: {title, description, user, notes, date }} = this.props
     return (
-      <div 
-        className='SnippetIndexCard' 
-        onClick={e => {
-          // debugger
-          this.props.handleClick(e)
-        }} 
-        snippetid={snippetId}
-      >
-        <p>----------------------</p>
-        <p>{title}</p>
-        <p>{description}</p>
-        <p>{user}</p>
-        {/* <p>{public}</p> */}
-        {/* <p>{notes}</p> */}
-        <p>{date}</p>
-        <p>---------------------</p>
+      <div className='snippet-index-card-container'>
+        <div 
+          className='snippet-index-card' 
+          onClick={e => {
+            // debugger
+            this.props.handleClick(e)
+          }} 
+          snippetid={snippetId}
+        >
+          <SnippetDisplayContainer snippet={this.props.snippet.notes} />
+          
+          <div className='snippet-index-card-base-container'>
+            <div className='snippet-index-card-title'>
+              <h4>{title}</h4>
+            </div>
+            <div className='snippet-index-card-description'>
+              <p>{description}</p>
+            </div>
+            
+            {/* <p>{user}</p> */}
+            {/* <p>{public}</p> */}
+            {/* <p>{notes}</p> */}
+            {/* <p>{date}</p> */}
+
+          </div>
+          
+        </div>
       </div>
     )
   }
