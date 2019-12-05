53dbd16935ed38b1c2a2473f146ba81211531d83
Phil-1041 <philzheng92@gmail.com>
2019-12-04 20:47:05 -0800

    added aws keys, prod and dev

--- config/dev_keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  S3_BUCKET: "museic-dev",
-  AWS_ACCESS_KEY_ID: "AKIAJ7M6DORAGDKQLGUA",
-  AWS_SECRET_ACCESS_KEY: "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",
-  REGION: "us-west-1"
-}
