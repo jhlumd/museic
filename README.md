bac5c28ac3d56ceba66748a843ba97d22c139295
Jaehyuk Lee <jhlumd@gmail.com>
2020-04-11 08:40:50 -0700

    Refactor snippet bar

--- README.md
+++ README.md
@@ -34,6 +34,7 @@ You can't predict when and where you'll be inspired with the next great tune. Wh
 
 ### Interactive splash tutorial page
 ![](demo/museic_splash.gif)
+
 Our splash page walks new users through a curated journey from intro to creating an account. 
 The steps are as follows:
 1. Captivating animated name, and description of app.
@@ -56,52 +57,12 @@ The input component is paired with a display component that dynamically creates 
 ```javascript
 function SnippetBar(props) {
   const { pitch, startTime, duration } = props;
-  let barTop;
-  switch (pitch) {
-    case "C6":
-      barTop = 0;
-      break;
-    case "B5":
-      barTop = 1;
-      break;
-    case "A#5":
-      barTop = 2;
-      break;
-    case "A5":
-      barTop = 3;
-      break;
-    case "G#5":
-      barTop = 4;
-      break;
-    case "G5":
-      barTop = 5;
-      break;
-    case "F#5":
-      barTop = 6;
-      break;
-    case "F5":
-      barTop = 7;
-      break;
-    case "E5":
-      barTop = 8;
-      break;
-    case "D#5":
-      barTop = 9;
-      break;
-    case "D5":
-      barTop = 10;
-      break;
-    case "C#5":
-      barTop = 11;
-      break;
-    case "C5":
-      barTop = 12;
-      break;
-    default:
-      barTop = 12;
-      break;
-  }
 
+  const pitchToNum = {
+    C6: 0, B5: 1, "A#5": 2, A5: 3, "G#5": 4, G5: 5, "F#5": 6,
+    F5: 7, E5: 8, "D#5": 9, D5: 10, "C#5": 11, C5: 12,
+  };
+  
   const noteStyle = {
     top: (barTop / 13) * 100 + "%",
     left: (startTime / 32) * 100 + "%",
--- frontend/src/components/snippet_display/_snippet_bar.jsx
+++ frontend/src/components/snippet_display/_snippet_bar.jsx
@@ -1,64 +1,30 @@
-import React from 'react';
+import React from "react";
 
 export default function SnippetBar(props) {
   const { pitch, startTime, duration } = props;
-  let barTop;
-  switch (pitch) {
-    case "C6":
-      barTop = 0;
-      break;
-    case "B5":
-      barTop = 1;
-      break;
-    case "A#5":
-      barTop = 2;
-      break;
-    case "A5":
-      barTop = 3;
-      break;
-    case "G#5":
-      barTop = 4;
-      break;
-    case "G5":
-      barTop = 5;
-      break;
-    case "F#5":
-      barTop = 6;
-      break;
-    case "F5":
-      barTop = 7;
-      break;
-    case "E5":
-      barTop = 8;
-      break;
-    case "D#5":
-      barTop = 9;
-      break;
-    case "D5":
-      barTop = 10;
-      break;
-    case "C#5":
-      barTop = 11;
-      break;
-    case "C5":
-      barTop = 12;
-      break;
-    default:
-      barTop = 12;
-      break;
-  }
 
-  const noteStyle = {
-    top: (barTop / 13) * 100 + "%",
-    left: (startTime / 32) * 100 + "%",
-    width: (duration / 32) * 100 + "%",
-    backgroundColor: `rgb(${props.backgroundColor[0]}, ${props.backgroundColor[1]}, ${props.backgroundColor[2]})`
+  const pitchToNum = {
+    C6: 0,
+    B5: 1,
+    "A#5": 2,
+    A5: 3,
+    "G#5": 4,
+    G5: 5,
+    "F#5": 6,
+    F5: 7,
+    E5: 8,
+    "D#5": 9,
+    D5: 10,
+    "C#5": 11,
+    C5: 12,
   };
 
-  return (
-    <div
-      className="single-note hvr-float"
-      style={noteStyle}
-    ></div>
-  );
+  const noteStyle = {
+    top: ((pitchToNum[pitch] || 12) / 13) * 100 + "%",
+    left: (startTime / 32) * 100 + "%",
+    width: (duration / 32) * 100 + "%",
+    backgroundColor: `rgb(${props.backgroundColor[0]}, ${props.backgroundColor[1]}, ${props.backgroundColor[2]})`,
+  };
+
+  return (<div className="single-note hvr-float" style={noteStyle}></div>);
 }
